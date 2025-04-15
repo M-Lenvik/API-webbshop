c9663673d85da32b678d3c73809d809944cfad09
Marie <lenvik.marie@gmail.com>
2025-04-15 21:00:22 +0200

    Add files via upload

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import express from 'express'; // Import express
+const app = express(); // Create an instanse
+
+//startar servern
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
